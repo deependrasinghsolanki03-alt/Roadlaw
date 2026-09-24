0c7e1dad734c5e66b6433e4c7840675600f3b398
deependrasinghsolanki03-alt <deependrasinghsolanki03@gmail.com>
2026-09-24 09:40:51 +0530

    Fix: Remove langchain_huggingface import

--- ss2_engine.py
+++ ss2_engine.py
@@ -38,7 +38,7 @@ HF_API_KEY = os.getenv("HF_API_KEY", "")
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.retrievers import BM25Retriever
-from langchain_huggingface import HuggingFaceEmbeddings
+# (HuggingFaceEmbeddings import removed)
 from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 from langchain_core.documents import Document
